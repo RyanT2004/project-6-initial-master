7310bcb4988c8868fb9a580ceec4d57cf377ccde
RyanT2004 <ryanchau2004@gmail.com>
2023-05-09 12:16:01 -0700

    Initial commit

---
+++ aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
@@ -0,0 +1,95 @@
+package com.example.aggregator.controller;
+
+import com.example.aggregator.model.Entry;
+import com.example.aggregator.service.AggregatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AggregatorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AggregatorController.class.getName());
+    private AggregatorService service;
+
+    public AggregatorController(AggregatorService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/getDefinitionFor/{word}")
+    public Entry getDefinition(@PathVariable String word){
+
+        StopWatch sw = new StopWatch();
+        sw.start();
+        Entry entry = service.getDefinitionFor(word);
+        sw.stop();
+
+        long nanoSeconds = sw.getLastTaskTimeNanos();
+        String message = new StringBuilder().append("Retrieved entries for words starting with [")
+                .append(word)
+                .append("] containing ")
+                .append(" entries in ")
+                .append(nanoSeconds / 1000000.0)
+                .append("ms")
+                .toString();
+
+        logger.info(message);
+
+        return entry;
+
+    }
+
+    @GetMapping("/getWordsThatContainSuccessiveLettersAndStartsWith/{chars}")
+    public List<Entry> getWordsThatContainSuccessiveLettersAndStartsWith(@PathVariable String chars){
+
+        StopWatch sw = new StopWatch();
+        sw.start();
+        List<Entry> entries = service.getWordsThatContainSuccessiveLettersAndStartsWith(chars);
+        sw.stop();
+
+        long nanoSeconds = sw.getLastTaskTimeNanos();
+        String message = new StringBuilder().append("Retrieved entries for words starting with [")
+                .append(chars)
+                .append("] containing ")
+                .append(" entries in ")
+                .append(entries.size())
+                .append(nanoSeconds / 1000000.0)
+                .append("ms")
+                .toString();
+
+        logger.info(message);
+
+        return entries;
+    }
+
+    @GetMapping("/getAllPalindromes/")
+    public List<Entry> getAllPalindromes(){
+
+        StopWatch sw = new StopWatch();
+        sw.start();
+        List<Entry> entry = service.getAllPalindromes();
+        sw.stop();
+
+        long nanoSeconds = sw.getLastTaskTimeNanos();
+        String message = new StringBuilder().append("Retrieved entries for palindrome words, ")
+                .append(" Entries received in ")
+                .append(nanoSeconds / 1000000.0)
+                .append(" ms ")
+                .toString();
+
+        logger.info(message);
+
+        return entry;
+
+    }
+}
---
+++ aggregator/src/main/java/com/example/aggregator/model/Example.java
@@ -0,0 +1,53 @@
+package com.example.aggregator.model;
+
+import com.fasterxml.jackson.annotation.*;
+
+import javax.annotation.Generated;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+"word",
+"definition"
+})
+@Generated("jsonschema2pojo")
+public class Example {
+
+    @JsonProperty("word")
+    private String word;
+    @JsonProperty("definition")
+    private String definition;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("word")
+    public String getWord() {
+        return word;
+    }
+
+    @JsonProperty("word")
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    @JsonProperty("definition")
+    public String getDefinition() {
+        return definition;
+    }
+
+    @JsonProperty("definition")
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
